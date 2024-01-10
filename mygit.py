ec0d6e120011d2b269e08c9279265d69a56ea5fe
nerov103 <nibir01000@gmail.com>
2024-01-10 11:33:39 +0600

    for example

--- mygit.py
+++ mygit.py
@@ -8,34 +8,33 @@ repo_url = str(input('enter your repo.Url[+]'))
 commit_mess = f"\"{re_commit_mess}\""
 # repo_url = "https://github.com/nerov103/example.git"
 
-def github():
-    step_0 = "git init"
-    step_1 = "git add ."
-    step_2 = f"git commit -m {commit_mess}" #enter your commit meassge
-    step_3 = "git branch -M main"
-    step_4 = f"git remote add origin {repo_url}" #enter your reponame
-    step_5 = "git push -u -f origin main"
+step_0 = "git init"
+step_1 = "git add ."
+step_2 = f"git commit -m {commit_mess}" #enter your commit meassge
+step_3 = "git branch -M main"
+step_4 = f"git remote add origin {repo_url}" #enter your reponame
+step_5 = "git push -u -f origin main"
 
-    clik = pyautogui.moveTo(800, 1000)
-    pyautogui.click(clik)
+clik = pyautogui.moveTo(800, 1000)
+pyautogui.click(clik)
 
 
-    #stetment for git init command
-    time.sleep(2)
-    if os.access(".git", os.F_OK):
-        print("exists .git")
-    else:
-        pyautogui.write(step_0)
-        pyautogui.press('enter')
-        time.sleep(0.5)
+#stetment for git init command
+time.sleep(2)
+if os.access(".git", os.F_OK):
+    print("exists .git")
+else:
+    pyautogui.write(step_0)
+    pyautogui.press('enter')
+    time.sleep(0.5)
     #None error in git  add . command
-    pyautogui.write(step_1)
-    pyautogui.press('enter')
-    time.sleep(0.5)
-    #stetment for git commit meassge
-    pyautogui.write(step_2)
-    pyautogui.press('enter')
-    time.sleep(0.5)
+pyautogui.write(step_1)
+pyautogui.press('enter')
+time.sleep(0.5)
+#stetment for git commit meassge
+pyautogui.write(step_2)
+pyautogui.press('enter')
+time.sleep(0.5)
     # print(" plaess double quotation marks "" in the commit meassge")
 
     #eror handler in exists repo
@@ -48,39 +47,32 @@ def github():
     #         pyautogui.write(step_3)
     #         pyautogui.press('enter')
     #         time.sleep(0.5)
-    pyautogui.write(step_3)
-    pyautogui.press('enter')
-    time.sleep(0.5)
+pyautogui.write(step_3)
+pyautogui.press('enter')
+time.sleep(0.5)
 
-    pyautogui.write(step_4)
-    pyautogui.press('enter')
-    time.sleep(0.5)
+pyautogui.write(step_4)
+pyautogui.press('enter')
+time.sleep(0.5)
 
-    pyautogui.write(step_5)
-    pyautogui.press('enter')
-    time.sleep(0.5)
-
-
+pyautogui.write(step_5)
+pyautogui.press('enter')
+time.sleep(0.5)
 
 
-    print("=================================")
-    print(step_0)
-    print(step_1)
-    print(step_2)
-    print(step_3)
-    print(step_4)
 
-    mess = "Code Run in Successly"
-    for i in mess:
-        print(i, end="", flush=True)
-        time.sleep(0.2)
 
-    print("Success!")
+print("=================================")
+
+    # mess = "Code Run in Successly"
+    # for i in mess:
+    #     print(i, end="", flush=True)
+    #     time.sleep(0.2)
+
+    # print("Success!")
 # print(commit_mess)
 
 # getdir = os.getcwd()
 # ro_path = os.path.join(getdir, ".git")
 # rmtree(ro_path)
 
-
-github()
